5f92f6f389c4eacd3897b5753158debe27bd3fa8
shraddha salvi <shraddhasalvi272@gmail.com>
2025-04-19 00:59:03 +0530

    completed proposal page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router,Route,Routes } from "react-router-dom";
 import ClientsideSidebar from "./Pages/ClientsideSidebar"
+import ProposalsPage from "./Pages/ProposalsPage";
 function App() {
   return (
     <Router>
     <Routes>
             <Route path="/ClientSidebar" element={<ClientsideSidebar/>} />
+            <Route path="/proposalspage" element={<ProposalsPage/>}/>
       </Routes>
     </Router>
   );
