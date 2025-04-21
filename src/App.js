853043e07d2b65d49a4953de30ed21ad5f3d9982
shraddha salvi <shraddhasalvi272@gmail.com>
2025-04-21 11:31:26 +0530

    Resolved merge conflict in App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import { BrowserRouter as Router,Route,Routes } from "react-router-dom";
 import ClientsideSidebar from "./Pages/ClientsideSidebar";
+<<<<<<< HEAD
 import ProposalsPage from "./Pages/ProposalsPage";
+=======
+import  EmployeeSignup from   "./Pages/EmployeeSignup";
+import  EmployeeLogin  from "./Pages/EmployeeLogin";
+import ProjectDashboard from './components/ProjectDashborad';
+
+>>>>>>> origin/master
 function App() {
   return (
+  
     <Router>
     <Routes>
             <Route path="/ClientSidebar" element={<ClientsideSidebar/>} />
+<<<<<<< HEAD
             <Route path="/proposalspage" element={<ProposalsPage/>}/>
+=======
+            <Route path="/EmployeeSignup" element={<EmployeeSignup/>} />
+            <Route path="/EmployeeLogin" element={<EmployeeLogin/>} />
+>>>>>>> origin/master
       </Routes>
     </Router>
+
+<ProjectDashboard/>
   );
 }
 
