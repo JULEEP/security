8efb7dbf9f0cb3a5ae8b59411451b3d027107c90
shraddha salvi <shraddhasalvi272@gmail.com>
2025-04-21 11:09:06 +0530

    resolve conflict

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router,Route,Routes } from "react-router-dom";
-import ClientsideSidebar from "./Pages/ClientsideSidebar"
+import ClientsideSidebar from "./Pages/ClientsideSidebar";
 import ProposalsPage from "./Pages/ProposalsPage";
 function App() {
   return (
