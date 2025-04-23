12730ef797cae3dd70f18a7030035e25f2e99f55
shyam sahu <sahushyam50380@gamil.com>
2025-04-23 19:25:24 +0530

    admin pages

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import { BrowserRouter as Router,Route,Routes } from "react-router-dom";
 import ClientsideSidebar from "./Pages/ClientsideSidebar";
 import  EmployeeSignup from   "./Pages/EmployeeSignup";
 import  EmployeeLogin  from "./Pages/EmployeeLogin";
-
+import ProjectDashboard from "./Dashboard/ProjectDashborad";
+import AdminSignup from "./Pages/AdminSignup";
+import AdminLogin from "./Pages/AdminLogin";
 function App() {
   return (
   
@@ -11,7 +13,9 @@ function App() {
             <Route path="/ClientSidebar" element={<ClientsideSidebar/>} />
             <Route path="/EmployeeSignup" element={<EmployeeSignup/>} />
             <Route path="/EmployeeLogin" element={<EmployeeLogin/>} />
-            <Route path="/projectDashboard" element={<ClientsideSidebar/>} />
+            <Route path="/projectDashboard" element={< ProjectDashboard/>} />
+            <Route path="/AdminSignup" element={<AdminSignup/>} />
+            <Route path="/AdminLogin" element={<AdminLogin/>} />
       </Routes>
     </Router>
 
