5e9c5bf332af60293042b4bd22ddf0e7da0e0145
pranavi2 <pranavirao14@gmail.com>
2025-04-24 21:50:44 +0530

    Initial commit with ReportDashboard layout and components

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import ClientsideSidebar from "./Pages/ClientsideSidebar";
 import EmployeeSignup from "./Pages/EmployeeSignup";
 import EmployeeLogin from "./Pages/EmployeeLogin";
 import Proposals from "./Pages/Proposals";
+import ReportDashboard from "./Pages/ReportDashboard";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Proposals />} /> {/* default route */}
+        {/* ✅ Dashboard is now the default route */}
+        <Route path="/" element={<ReportDashboard />} />
+        <Route path="/dashboard" element={<ReportDashboard />} />
         <Route path="/ClientSidebar" element={<ClientsideSidebar />} />
         <Route path="/EmployeeSignup" element={<EmployeeSignup />} />
         <Route path="/EmployeeLogin" element={<EmployeeLogin />} />
